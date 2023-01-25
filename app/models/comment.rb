56d116ade64f87fe2ad2c13df4d51e6aa49983c4
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-01-25 10:00:06 +0100

    Create custom method and model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
- belongs_to :post
- belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
- def update_comments_counter
+  def update_comments_counter
     post.increment!(:comments_couter)
- end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts,  foreign_key: 'author_id'
-  has_many :comments,  foreign_key: 'author_id'
-  has_many :likes,  foreign_key: 'author_id'
+  has_many :posts, dependent: :destroy, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   def last_three_posts
     posts.last(3)
