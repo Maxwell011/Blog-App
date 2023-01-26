8287b282a6ffa79c5a3b9e94133047fc6c7470d3
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-01-26 16:20:13 +0100

    Added validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,6 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   def update_likes_counter
-    update(likes_counter: likes.count)
+    post.increment!(:likes_counter)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,13 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'posts_id'
   has_many :comments, foreign_key: 'posts_id'
   belongs_to :author, class_name: 'User', dependent: :destroy, foreign_key: 'author_id'
+  after_save :update_post_counter
 
-  def update_posts_couter
+  validates :title, presence: true, length: { in: 1..250 }
+  validates :author_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def update_posts_counter
     author.increment!(:posts_counter)
   end
 
