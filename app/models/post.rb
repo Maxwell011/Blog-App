161643f6817639a441963651ed93b1a60a62cd42
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-02-03 10:34:07 +0100

    Fix changes reviewer requested

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  has_many :likes, foreign_key: 'posts_id'
-  has_many :comments, foreign_key: 'posts_id'
+  has_many :likes, foreign_key: 'post_id'
+  has_many :comments, foreign_key: 'post_id'
   belongs_to :author, class_name: 'User', dependent: :destroy, foreign_key: 'author_id'
   after_save :update_post_counter
 
